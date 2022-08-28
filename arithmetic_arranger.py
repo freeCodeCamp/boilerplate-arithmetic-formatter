d62038b767b9bacc4e1690b5b206807ad102eca6
youssef ben rejeb <youssef.benrejeb@gmail.com>
2022-08-28 20:12:38 +0100

    adding error handeler

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,43 +1,37 @@
 def arithmetic_arranger(problems):
-    maList = []
-    tab = []
+    args = []
+
+    len_problems = len(problems)
+    if len_problems > 5:
+        print("Error: Too many problems.")
+        return
+
     for problem in problems:
         args = problem.split()
-        for i in range(3):
-            if args[i] not in ["+", "-"]:
-                maList.append(args[i])
+        if args[1] in ["%", "*"]:
+            print("Error: Operator must be '+' or '-'.")
+            return
+        if not args[0].isdigit() and args[2].isdigit():
+            print("Error: Numbers must only contain digits.")
+            return
+        if len(args[0]) > 4 or len(args[2]) > 4:
+            print("Error: Numbers cannot be more than four digits.")
+            return
 
-    # connaitre le plus grand des deux valeurs
-    print(maList)
-    tail = len(maList)
-    print(tail)
-    comp = [0, 2, 4, 6]
-    for i in comp:
-        print(maList[i], maList[i + 1])
-        tab.append(len(max(maList[i], maList[i + 1], key=len)))
+            # return arranged_problems
 
-    print(tab)
-    for c, t in zip(comp, tab):
-        print(f"{maList[c]:>{t + 6}s}", end="")
-    print("\n")
-    comp2 = [1, 3, 5, 7]
-    for c2, t in zip(comp2, tab):
-        print(f"{'+':>{tab[t]}s} {maList[c2]}", end="    ")
+    for problem in problems:
+        args = problem.split()
+        longest = len(max(args[0], args[2], key=len))
 
-
-#     for i in len(maList):
-#         if maList[i]<maList[i+2]:
-#             tab.append(len(maList[i+2]))
-
-# tab = abs(len(args[0]) - len(args[1]))
-# maxLen = len(args[2])
-
-# if len(args[0]) < len(args[2]):
-#     print(f"{args[0]:>{maxLen + 2}s}\n{args[1]} {args[2]}")
-
-# return arranged_problems
+        # if len(args[0]) <= len(args[2]):
+        #     print(f"{args[0]:>{longest + 2}s}")
+        #     print(f"{args[1]} {args[2]}")
+        # else:
+        print(f"{args[0]:>2}")
+        # print(f"{args[1]}{args[2]:>{longest + 2}s}")
 
 
 if __name__ == '__main__':
-    arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
-    #
+    # arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+    arithmetic_arranger(["132 + 68"])

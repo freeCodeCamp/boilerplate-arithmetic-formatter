ab081e3177fcb25ed78780a0e085158a9c3b96f1
Gauti-Pwr <131699373+Gauti-Pwr@users.noreply.github.com>
2023-11-20 21:16:56 +0530

    Update arithmetic_arranger.py

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,11 @@
 def arithmetic_arranger(problems):
-
+    length = len(problems)
+    for item in problems:
+        sub_items = item.split()
+        first_op = first_op.append(subitems[0])
+        operand = operand.append(subitems[1])
+        second_op = second_op.append(subitems[2])
+        
+    
 
     return arranged_problems
